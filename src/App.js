8cc2dc23358c38637c5414ab51364456e84edd3c
rddv <d.razumnyi@andersenlab.com>
2018-08-14 15:29:02 +0300

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
         <Provider>
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <div className="App">
                     <Header/>
                     <div className="container">
